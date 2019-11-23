dcb1808f613e0095c4ca7379aaaedf98106fda2a
Vincent Nikolayev <vincent@Vincents-MacBook-Pro.local>
2019-11-23 03:34:04 -0500

    documentation

--- basic_metadata.py
+++ basic_metadata.py
@@ -11,13 +11,14 @@ from typing import Union, Callable, Dict, Iterable
 
 spark_col = col
 
-OUTPUT_KEY = 'columns'
+OUTPUT_KEY = 'columns' # key mapped to a dataset which holds the column output (like column metadata)
 
 
 def map_cols(df: DataFrame) -> Column:
     """
-    maps column names to column values using Spark's MapType. A column of Maps is returned where each row represents a map like {column_name: [column_vals]}.
-    then explodes (flattens) the output of map_cols so that each row maps one column_name to one column_value.
+    maps column names to column values using Spark's MapType. A column of Spark "Maps" is returned where each row represents a Map like {column_name: [column_vals]...}.
+    It then explodes (flattens) the output of map_cols so that each row maps one column_name to one column_value, i.e. [col1: val1, col1: val2...]
+    Spark "Column" (of dataset columns) is returned 
     """
 
     df_mapped_cols = df.select(
@@ -36,77 +37,98 @@ def map_cols(df: DataFrame) -> Column:
     return df_exploded_cols
 
 
-def reduce_col(df_cols: Union[DataFrame, Column],
+def reduce_cols(df_cols: Union[DataFrame, Column],
                agg_name: str, aggFunc: Callable, other_groupBy_keys: Iterable[Column] = []) -> DataFrame:
     """
-    accepts map_cols output and groups by column name using the aggFunc.
-    agg_name is later in append_output as a key to get the outputs out. 
-    other_groupBy_keys is a list of other groupby keys and is used in cases such as getting the top 5 frequent vals
+    accepts map_cols output (flattened columns of columns) and groups by column name using the aggFunc.
+    agg_name is later used in append_output as a key to get the outputs and add them to the output dict. 
+    other_groupBy_keys is used in cases such as getting the top 5 frequent vals
     """
     return df_cols.groupBy(
         spark_col("key").alias("col_name"), *other_groupBy_keys).agg(
         aggFunc("value").alias(agg_name))
 
 
-def append_output(master_dct: Dict, key: str, df_output: DataFrame):
+def append_output(ds_dct: Dict, result_key: str, df_output: DataFrame):
     """
-    adds key (a column_name in df_output) to master_dct, where it is mapped to a column name. is used for the output of reduce_col
+    adds result_key (a column_name in df_output) to ds_dct (output dict for a dataset), where it is mapped to the appropriate column name. 
+    is used for the output of reduce_cols.
     """
     output_rows = df_output.collect()
     for row in output_rows:
         col_name = row['col_name']
-        if not col_name in master_dct[OUTPUT_KEY]:
-            master_dct[OUTPUT_KEY][col_name] = {'column_name': col_name}
-        # if key already in dct then change val into list and append value
-        if key in master_dct[OUTPUT_KEY][col_name]:
-            if not isinstance(master_dct[OUTPUT_KEY][col_name][key], list):
-                master_dct[OUTPUT_KEY][col_name][key] = [
-                    master_dct[OUTPUT_KEY][col_name][key]]
-            master_dct[OUTPUT_KEY][col_name][key].append(row[key])
-        # if key not in dct then set key = val
+        if not col_name in ds_dct[OUTPUT_KEY]:
+            ds_dct[OUTPUT_KEY][col_name] = {'column_name': col_name}
+        # if result_key already in dct then change val into list and append value (like for frequent values)
+        if result_key in ds_dct[OUTPUT_KEY][col_name]:
+            if not isinstance(ds_dct[OUTPUT_KEY][col_name][result_key], list):
+                ds_dct[OUTPUT_KEY][col_name][result_key] = [
+                    ds_dct[OUTPUT_KEY][col_name][result_key]]
+            ds_dct[OUTPUT_KEY][col_name][result_key].append(row[result_key])
+        # if result_key not in dct then set result_key = val
         else:
-            master_dct[OUTPUT_KEY][col_name][key] = row[key]
+            ds_dct[OUTPUT_KEY][col_name][result_key] = row[result_key]
 
 
-def get_count_distincts(df_cols: Union[DataFrame, Column], master_dct: Dict):
-    rows = reduce_col(df_cols, 'number_distinct_values',
+def get_count_distincts(df_cols: Union[DataFrame, Column], ds_dct: Dict):
+    df_output = reduce_cols(df_cols, 'number_distinct_values',
                       countDistinct)
-    append_output(master_dct, 'number_distinct_values', rows)
+    append_output(ds_dct, 'number_distinct_values', df_output)
 
 
-def get_top_5(df_cols: Union[DataFrame, Column], master_dct: Dict):
-    rows = reduce_col(df_cols, 'count', count, other_groupBy_keys=[
+def get_top_5(df_cols: Union[DataFrame, Column], ds_dct: Dict):
+    df_output = reduce_cols(df_cols, 'count', count, other_groupBy_keys=[
                       spark_col('value').alias("frequent_values")])
 
-    counts = rows.sort('count', ascending=False)  # speed
+    counts = df_output.sort('count', ascending=False)  # sort only on count for speed
     # counts.sort('col_name', 'count', ascending=False).show()  # DEBUG
 
-    rows = counts.groupBy('col_name').agg(
+    df_output = counts.groupBy('col_name').agg(
         collect_list('frequent_values').alias('frequent_values'))
     top_5 = udf(lambda lst: [str(val)
-                             for val in lst[:5]], ArrayType(StringType()))
-    rows = rows.select('col_name', top_5(
+                             for val in lst[:5]], ArrayType(StringType())) # take top 5
+    df_output = df_output.select('col_name', top_5(
         'frequent_values').alias('frequent_values'))
-    append_output(master_dct, 'frequent_values', rows)
+    append_output(ds_dct, 'frequent_values', df_output)
 
 
-def get_non_empty(df_cols: Union[DataFrame, Column], master_dct: Dict):
-    rows = reduce_col(df_cols, 'number_non_empty_cells',
+def get_non_empty(df_cols: Union[DataFrame, Column], ds_dct: Dict):
+    df_output = reduce_cols(df_cols, 'number_non_empty_cells',
                       lambda c: count(when(~(isnan(c) | isnull(c)), c)))
-    append_output(master_dct, 'number_non_empty_cells', rows)
+    append_output(ds_dct, 'number_non_empty_cells', df_output)
 
 
-def get_empty(df_cols: Union[DataFrame, Column], master_dct: Dict):
-    rows = reduce_col(df_cols, 'number_empty_cells',
+def get_empty(df_cols: Union[DataFrame, Column], ds_dct: Dict):
+    df_output = reduce_cols(df_cols, 'number_empty_cells',
                       lambda c: count(when(isnan(c) | isnull(c), c)))
-    append_output(master_dct, 'number_empty_cells', rows)
+    append_output(ds_dct, 'number_empty_cells', df_output)
 
 
-def get_basic_metadata(df_cols: Union[DataFrame, Column], master_dct: Dict):
-    get_count_distincts(df_cols, master_dct)
-    get_top_5(df_cols, master_dct)
-    get_non_empty(df_cols, master_dct)
-    get_empty(df_cols, master_dct)
+def get_basic_metadata(df_cols: Union[DataFrame, Column], ds_dct: Dict):
+    """
+    driver function that updates ds_dct with the following:
+    {
+        "columns": {
+            "<column_name>": {
+            "column_name": <string>,
+            "number_distinct_values": <int>,
+            "frequent_values": [
+                <string1>,
+                <string2>,
+                <string3>,
+                <string4>,
+                <string5>,
+            ],
+            "number_non_empty_cells": <int>, 
+            "number_empty_cells": <int>
+            ...
+        }
+    }
+    """
+    get_count_distincts(df_cols, ds_dct)
+    get_top_5(df_cols, ds_dct)
+    get_non_empty(df_cols, ds_dct)
+    get_empty(df_cols, ds_dct)
 
 
 def get_val_from_single_val_col(df: Column):
@@ -122,36 +144,36 @@ def __test(dfs):
     for i, df in enumerate(dfs):
         start = time.time()
 
-        master_dct = {
+        ds_dct = {
             'dataset': df.ds_name,
             OUTPUT_KEY: {}
         }
         df_cols = map_cols(df)
 
-        get_count_distincts(df_cols, master_dct)
+        get_count_distincts(df_cols, ds_dct)
 
         end = time.time()
         print(end-start)
-        print(master_dct)
+        print(ds_dct)
 
         ###
 
         start = time.time()
 
-        master_dct = {
+        ds_dct = {
             'dataset': df.ds_name,
             OUTPUT_KEY: {}
         }
         for col_name in df.columns:
             num_distincts = get_val_from_single_val_col(
                 df.agg(countDistinct(col_name)))
-            if not col_name in master_dct[OUTPUT_KEY]:
-                master_dct[OUTPUT_KEY][col_name] = {'column_name': col_name}
-            master_dct[OUTPUT_KEY][col_name]['number_distinct_values'] = num_distincts
+            if not col_name in ds_dct[OUTPUT_KEY]:
+                ds_dct[OUTPUT_KEY][col_name] = {'column_name': col_name}
+            ds_dct[OUTPUT_KEY][col_name]['number_distinct_values'] = num_distincts
 
         end = time.time()
         print(end-start)
-        print(master_dct)
+        print(ds_dct)
 
         if i >= limit - 1:
             exit(0)
@@ -168,20 +190,22 @@ if __name__ == '__main__':
 
     start_all_time = time.time()
 
+    # master_dct contains all output to be used in json
     master_dct = {}
     for i, df in enumerate(dfs):
         start = time.time()
 
         ### MAIN ###
+        # dct belongs to a dataset
         dct = {
             'dataset_name': df.ds_name,
             OUTPUT_KEY: {}
         }
         df_cols = map_cols(df)
-        get_basic_metadata(df_cols, dct)
+        get_basic_metadata(df_cols, dct) # main driver
         
         ### test num_cells (counts) ###
-        # rows = reduce_col(df_cols, 'number_cells', lambda c: sum(lit(1)))
+        # rows = reduce_cols(df_cols, 'number_cells', lambda c: sum(lit(1)))
         # append_output(dct, 'number_cells', rows)
 
         # for col in dct[OUTPUT_KEY].keys():
@@ -189,7 +213,7 @@ if __name__ == '__main__':
         #         raise ValueError('Error in dataset: {}. Column: {}'.format(
         #             dct.keys()[0], str(dct[OUTPUT_KEY][col])))
 
-        master_dct.update({dct['dataset_name']: dct})
+        master_dct.update({dct['dataset_name']: dct}) # update master_dct with dataset
 
         end = time.time()
         timed = end-start
