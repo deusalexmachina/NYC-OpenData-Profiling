8a3142a4bafe4750860a152402b2f164eacaff4b
Vincent Nikolayev <vincent@Vincents-MacBook-Pro.local>
2019-12-06 01:09:28 -0500

    attempted to package env (failing)

--- env.sh
+++ env.sh
@@ -1,8 +1,23 @@
 # loads env and run command
 module load python/gnu/3.6.5
 module load spark/2.2.0
-export PYTHON_PATH='/share/apps/python/3.6.5/bin/python'
 
-spark-submit --conf spark.pyspark.python=$PYTHON_PATH $1 
+# PYTHON_PATH='/share/apps/python/3.6.5/bin/python'
+
+PYTHON_PATH="$HOME/.conda/envs/big_data/bin/python"
+spark-submit \
+--conf spark.pyspark.python=$PYTHON_PATH \
+--conf spark.executor.memoryOverhead=6G \
+--executor-memory 6G \
+$1
+
+# # TODO: below doesn't work
+# PYTHON_PATH="./CONDA/big_data/bin/python"
+# spark-submit \
+# --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYTHON_PATH \
+# --archives big_data.zip#CONDA \
+# --conf spark.executor.memoryOverhead=6G \
+# --executor-memory 6G \
+# $1
 
 # execute like `[vn500@login-1-1 theBiggestData]$ ./env.sh "ds_reader.py datasets"`
