56b6b5b49bb0f1525c1757df7b46d524f5591852
Vincent Nikolayev <vincent@Vincents-MacBook-Pro.local>
2019-12-08 20:37:53 -0500

    integrated

---
+++ task1_driver.py
@@ -0,0 +1,67 @@
+from timing import timed
+
+from pyspark.sql import SparkSession
+
+from basic_metadata import map_cols, get_basic_metadata, OUTPUT_KEY
+from basic_profile import get_dataset_profile
+from compile_itemset import combine_itemsets, most_frequent_itemsets
+
+from os import makedirs
+from os.path import isdir
+
+import json
+
+
+def append_data_profile(ds_dct, dct_output):
+    for col, dct in dct_output.items():
+        if not 'datatypes' in ds_dct[OUTPUT_KEY][col]:
+            ds_dct[OUTPUT_KEY][col]['datatypes'] = {}
+        ds_dct[OUTPUT_KEY][col]['datatypes'].update(dct)
+
+
+def run():
+    spark = SparkSession.builder.getOrCreate()
+
+    # master_dct contains all output to be used in json
+    master_dct = {}
+    # master_itemset contains all itemsets to be used in json
+    master_itemset = {}
+    def _run(df, i):
+        # BASIC METADATA
+        # ds_dct belongs to a dataset
+        ds_dct = {
+            'dataset_name': df.ds_name,
+            OUTPUT_KEY: {}
+        }
+        df_cols = map_cols(df)
+        get_basic_metadata(df_cols, ds_dct)  # main driver
+
+        ### BASIC PROFILE ###
+        dct_output, dct_itemset = get_dataset_profile(spark, df_cols)
+        append_data_profile(ds_dct, dct_output)
+        print(ds_dct)
+        ds_dct['itemset'] = dct_itemset
+
+        # add the counts for df_itemset to master
+        combine_itemsets(dct_itemset, master_itemset)
+
+        ### DONE: update master_dct with dataset ###
+        master_dct.update({ds_dct['dataset_name']: ds_dct})
+
+        print("Master Itemset: ", master_itemset)
+        with open("master_itemset.json", "w") as json_file:
+            json.dump(master_itemset, json_file, indent=4)
+
+        return ds_dct
+
+    timed(_run)
+
+    result_set = most_frequent_itemsets(master_itemset)
+    print("Most Frequent Itemsets: ", result_set)
+
+
+if __name__ == '__main__':
+    if not isdir('results_times'):
+        makedirs('results_times')
+
+    run()
--- timing.py
+++ timing.py
@@ -5,6 +5,8 @@ from pyspark.sql import utils
 
 import time
 import sys
+import json
+import numpy as np
 
 from typing import List
 
@@ -23,6 +25,10 @@ def timed(fn, files: List[str] = None) -> None:
     example from cli: `./env.sh "basic_metadata.py - True"`
     """
     spark = SparkSession.builder.getOrCreate()  # init spark
+
+    spark.sparkContext.setLogLevel("ERROR") # Warnings cluttered stdout
+    run_time_record = np.array([])
+    average_time_record = np.array([])
 
     time_start_all = time.time()
     time_end = time_start_all
@@ -75,15 +81,29 @@ def timed(fn, files: List[str] = None) -> None:
         total_runtime_load = time_end - time_start_all
         print('running avg runtime (including load):', total_runtime_load / actual_len_dfs)
 
+        # Save the output for the df to json for each run
+        with open("results_times/master_dct.json", "a+") as json_file:
+            json.dump(output, json_file, indent=4)
+            json_file.write("\n")
+
+        # keep a track of run times for visualization later
+        run_time_record = np.append(run_time_record, [runtime])
+        average_time_record = np.append(average_time_record, [total_runtime_load / actual_len_dfs])
+        # ----
+
         if runtime > max_runtime:
             max_runtime = runtime
             max_output = output
 
-        if i >= limit:
+        if actual_len_dfs >= limit:
             break
 
     total_runtime_load = time_end - time_start_all
 
+    np.savetxt("results_times/run_time_record", run_time_record,
+               delimiter=",")  # Save run times to csv
+    np.savetxt("results_times/average_time_record", average_time_record, delimiter=",") # Save avg run time to  csv
+    # --- 
     print()
     print(f"### SUMMARY FOR {actual_len_dfs} DATASETS ###")
     print("max runtime:", max_runtime)
