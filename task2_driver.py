ded17d6cc363602e7d0a6abd2434a3d7e8f6c268
Vincent Nikolayev <vincent@Vincents-MacBook-Pro.local>
2019-12-17 04:08:20 -0500

    commented out code to simplify

--- task2_driver.py
+++ task2_driver.py
@@ -29,14 +29,14 @@ ours:
 
 from sample import get_task2_dfs
 from cli import get_ds_path_arg, get_rand_arg, get_sort_arg
-from similarity import match_preprocess, match_jacc_avg, S_TYPE, ADJ_DIST, DIST, MIN_DIST, AVG_DIST, COL
+from similarity import match_preprocess, match_jacc_avg, S_TYPE, MIN_DIST, AVG_DIST, COL
 from timing import timed
 from basic_metadata import map_cols
 from ds_reader import get_ds_file_sizes
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col, lit, count as spark_count
-from pyspark.sql.types import StringType
+from pyspark.sql.types import StringType, StructType, StructField, BooleanType
 
 import random
 
@@ -68,12 +68,11 @@ def collect(df: pd.DataFrame, col_key: str, col_val: str) -> Dict[str, List[str]
 
 
 def _default(s_type, col_val):
-    return (s_type, col_val, 0.1, 0.1)
+    return (s_type, col_val, 0.2, 0.2)
 
 
 def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
     spark = SparkSession.builder.getOrCreate()
-    sc = spark.sparkContext
 
     if not os.path.isdir('results_similarities'):
         os.makedirs('results_similarities')
@@ -83,7 +82,7 @@ def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
         random.shuffle(gz_paths_cols)
     
     # unzip
-    gz_paths: List[str] = [gz_paths for gz_paths, _ in gz_paths_cols]
+    gz_paths: List[str] = [gz_paths for gz_paths, _, _ in gz_paths_cols]
 
     to_sort = get_sort_arg()
     if to_sort:
@@ -92,10 +91,11 @@ def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
         gz_paths_cols = list(zip(
             *sorted(list(zip(gz_paths_cols, sizes)),
                     key=lambda x: x[1])))[0]  # sort on sizes
-        gz_paths: List[str] = [gz_paths for gz_paths, _ in gz_paths_cols]
+        gz_paths: List[str] = [gz_paths for gz_paths, _, _ in gz_paths_cols]
 
     # unzip
-    cols: List[str] = [cols for _, cols in gz_paths_cols]
+    cols: List[str] = [cols for _, cols, _ in gz_paths_cols]
+    ta_path: List[str] = [ta_path for _, _, ta_path in gz_paths_cols]
 
     cache_col_name = {}  # {col_name: semantic_type} | may help if cols are repeated across dfs
     cache_col_val = {}  # there are many values repeated in a column so this helps
@@ -105,7 +105,7 @@ def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
     #         cache_col_name[col_name] = str(match_preprocess(col_name, ref_set_cols)[S_TYPE])
     #     return cache_col_name[col_name] 
 
-    def _match_semantic_vals(col_val, s_type_col):
+    def _match_semantic_vals(col_val, s_type_col, is_spark=True):
         """
         stage 1:
         run value matcher ('match_preprocess') on only the matched s_type_col
@@ -132,97 +132,122 @@ def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
         col_val = str(col_val)
         s_type_col = str(s_type_col)
 
+        add = False
+
         # print(col_val, s_type_col, {s_type_col: [ref_set_vals[s_type_col]]})
         if not col_val in cache_col_val:
             AVG_CUTOFF = 0.9  # similarity measure worse than this triggers second more general run
-            MIN_CUTOFF = 0.65
-            IDENTICAL_CUTOFF = 0.10
-            IMPROVE_RATIO = 0.5  # second run improved by some percent
+            # MIN_CUTOFF = 0.65
+            # IDENTICAL_CUTOFF = 0.10
+            IMPROVE_RATIO = 0.2  # second run improved by some percent
 
             str_col_val = str(col_val).lower()
             # print(str_col_val)
-            if str_col_val == 'null' or str_col_val == '-' or str_col_val == '0' or str_col_val == 'none' or str_col_val == '' or col_val is None:
+            if str_col_val == 'null' or str_col_val == '-' or str_col_val == '_' or str_col_val == '0' or str_col_val == 'none' or str_col_val == '' or col_val is None:
                 res_final = (s_type_col, col_val, 0.0, 0.0)  # default to s_type_col
             else:
                 res0 = match_preprocess(col_val, {s_type_col: ref_set_vals[s_type_col]}, match_jacc_avg)  # compare to values of matched (based on col_name) semantic type
                 # print('res0:', res0)
                 # res0[MIN_DIST] != 0.0
-                if AVG_CUTOFF < res0[AVG_DIST] or res0 is None:  # was the cutoff passed, i.e. was the value present for this semantic type based on the col_name match?
+                if res0 is None or AVG_CUTOFF < res0[AVG_DIST]:  # was the cutoff passed, i.e. was the value present for this semantic type based on the col_name match?
                     # check only these semantic types based on the content of the col_val (more explicit rules after examining data)
                     check = []
-                    if len(str_col_val) == 1 and str_col_val.isalpha():
+                    remove = []
+                    is_alpha = str_col_val.isalpha()
+                    is_digit = str_col_val.isdigit()
+                    if len(str_col_val) == 1 and is_alpha:
                         possibles = ['person_name (middle_initial)', 'borough']
                         for pos_s_type in possibles:
                             if s_type_col == pos_s_type:  # which of these is the s_type of the col?
                                 check.extend([pos_s_type])
                                 break
-                    if len(str_col_val) == 2 and str_col_val.isalpha():
+                    if len(str_col_val) == 2 and is_alpha:
                         check.extend(['color'])
+                    if len(str_col_val) == 5 and is_digit:
+                        check.extend(['zip_code'])
+                    if len(str_col_val) >= 3 and is_digit:
+                        check.extend(['city_agency', 'street_number',
+                                    'phone_number', 'building_classification'])
+                    if len(str_col_val) >= 1 and is_digit:
+                        check.extend(['street_number'])
+                    if 'ps ' in str_col_val or 'is ' in str_col_val or 'js ' in str_col_val or 'hs ' in str_col_val:
+                        check.extend(['school_name'])
                     if len(str_col_val) >= 3:  # can have numbers and other chars
                         if 'llc' in str_col_val or 'inc' in str_col_val or 'co' in str_col_val:
                             check.extend(['business_name'])
                         if 'http' in str_col_val or 'www' in str_col_val or 'org' in str_col_val or 'com' in str_col_val:
                             check.extend(['website'])
-                    if len(str_col_val) == 5 and str_col_val.isdigit():
-                        check.extend(['zip_code'])
-                    if len(str_col_val) >= 6 and 'school' in str_col_val:
-                        check.extend(['city_agency', 'street_number',
-                                    'phone_number', 'building_classification'])
-                    if len(str_col_val) >= 3 and str_col_val.isdigit():
-                        check.extend(['city_agency', 'street_number',
-                                    'phone_number', 'building_classification'])
-                    if len(str_col_val) >= 1 and str_col_val.isdigit():
-                        check.extend(['street_number'])
+                        if 'ave' in str_col_val or 'str' in str_col_val:
+                            if str_col_val[0].isdigit():
+                                check.extend(['address'])
 
                     # if len(check) > 0:
                     #     print('check:', check)
 
                     check = list(set(check))
+                    remove = list(set(remove))
                     
                     if len(check) == 0:
                         # compare to every semantic type but already checked
-                        ref_set_diff = copy.deepcopy(ref_set_vals)  # clone
-                        for key, val in ref_set_cols.items():  # compare to column names as well (for ms_core)
-                            ref_set_diff[key].extend(copy.deepcopy(val))
+                        if is_spark:  # check for expensive unnecessary operation
+                            ref_set_diff = ref_set_vals
+                        else:
+                            ref_set_diff = copy.deepcopy(ref_set_vals)  # clone
                     else:
                         # compare to only those in check
                         ref_set_diff = {}
                         for s_type in check:
-                            ref_set_diff[s_type] = copy.deepcopy(ref_set_vals[s_type])
-                    ref_set_diff[s_type_col] = []  # prevent key error and delete all values for already matched
+                            if is_spark:
+                                ref_set_diff[s_type] = ref_set_vals[s_type]
+                            else:
+                                ref_set_diff[s_type] = copy.deepcopy(ref_set_vals[s_type])
+                    # for key, val in ref_set_cols.items():  # compare to column names as well (for ms_core)
+                    #     if key in ref_set_diff:
+                    #         ref_set_diff[key].extend(val)
+                    # ref_set_diff[s_type_col] = []  # prevent key error and delete all values for already matched
+                    ref_set_diff[s_type_col] = ref_set_vals[s_type_col]  # prevent key error and delete all values for already matched
+                    for rm in remove:
+                        if rm in ref_set_diff:
+                            ref_set_diff[rm] = []
 
                     res1 = match_preprocess(col_val, ref_set_diff, match_jacc_avg)  # find similarity with other semantic value types
-                    
+                    res_final = res1
+
                     if res0 is None and res1 is None:
                         res_final = (s_type_col, col_val, 0.0, 0.0)
                     elif res0 is None:
+                        # print('res0:', res0, res1)
                         res_final = res1
                     elif res1 is None:
+                        # print('res1:', res0, res1)
                         res_final = res0
                     else: # neither are None
                         res_final = min([res0, res1], key=lambda x: x[AVG_DIST])
 
-                    # if AVG_CUTOFF < res_final[AVG_DIST]:  # still greater than cutoff and therefore unknown
-                    if not (res_final[AVG_DIST] < res0[AVG_DIST] * (1 - IMPROVE_RATIO)):  # dist has not improved
-                        res_final = _default(s_type_col, col_val)  # default to s_type_col
-                        # ^ should the distance be non-0 to add to ref_set?
+                        # if AVG_CUTOFF < res_final[AVG_DIST]:  # still greater than cutoff and therefore unknown
+                        if not (res_final[AVG_DIST] <= (res0[AVG_DIST] * (1 - IMPROVE_RATIO))):  # dist has not improved
+                            res_final = _default(s_type_col, col_val)  # default to s_type_col
+                            # ^ should the distance be non-0 to add to ref_set?
                 else:
                     # print('FALSE')
                     res_final = res0  # cutoff passed, return initial result
                 
-            # not an exact match and up to n different values stored
-            if res_final[MIN_DIST] <= MIN_CUTOFF and res_final[MIN_DIST] >= IDENTICAL_CUTOFF and len(ref_set_vals[res_final[S_TYPE]]) < 100:
-                ref_set_vals[res_final[S_TYPE]].append(col_val)  # append to ref_set
+            # # not an exact match and up to n different values stored
+            # if res_final[MIN_DIST] <= MIN_CUTOFF and res_final[MIN_DIST] >= IDENTICAL_CUTOFF and len(ref_set_vals[res_final[S_TYPE]]) < 30:
+            #     if is_spark:
+            #         add = True
+            #     else:
+            #         ref_set_vals[res_final[S_TYPE]].append(col_val)  # append to ref_set
             cache_col_val[col_val] = str(res_final[S_TYPE])
-            # print('res_final:', res_final)
+            # # print('res_final:', res_final)
 
-        return cache_col_val[col_val]
+        return (cache_col_val[col_val], add)
         
 
     # match_semantic_cols = udf(_match_semantic_cols, StringType())
-    match_semantic_vals = udf(_match_semantic_vals, StringType())
+    match_semantic_vals = udf(_match_semantic_vals, StructType([StructField('s_type_val', StringType(), False), StructField('add', BooleanType(), False)]))
 
-    master_dct = {}
+    master_lst = []
     def _run(df, i):
         print("col_name:", cols[i])
 
@@ -241,35 +266,51 @@ def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
             cache_col_name[col] = match_preprocess(col, ref_set_cols)[S_TYPE]  # match col to s_type
         s_type_col = cache_col_name[col]
 
-        print('s_type_col:', s_type_col)
-        print('ref_set_vals[s_type_col]:', ref_set_vals[s_type_col])
+        # print('s_type_col:', s_type_col)
+        # print('ref_set_vals[s_type_col]:', ref_set_vals[s_type_col])
 
         df_cols = df_cols.withColumn('s_type_col', lit(s_type_col))  # populate df with col s_type
-        
-        # if i < 35: # run on small datasets (before it gets slow)
-        s_types_all = []
-        ### Python method: no spark to add to ref_set_vals
-        for row in df_cols.select('value', 's_type_col').collect():
-            s_type_i = _match_semantic_vals(row['value'], row['s_type_col'])
-            s_types_all.append(s_type_i)
-        # get (s_type, count)
-        s_types_distinct = sc.parallelize(s_types_all).countByValue().items()
-        ###
 
-        # the below udf call just pulls out the s_types from the cache
-        df_cols = df_cols.withColumn('s_type_val', match_semantic_vals('value', 's_type_col'))  # match uknown col value to semantic type
-        df_test = df_cols.groupby('s_type_col', 'value', 's_type_val').count()
-        df_test = df_test.sort('count', ascending=False)
-        df_test.filter('s_type_val != s_type_col').show(25)
-        df_test.show(25)
-        # results = [str(list(row.asDict().values())) + '\n' for row in df_test.collect()]
-        # print(results[:10])
-        # with open('results_similarities/test.txt', '+a') as f:
-        #     for s in results:
-        #         f.write(s)
+        # s_types_distinct = [(s_type_col, df_cols.count())]
+        
+        # ### Python method: no spark to add to ref_set_vals
+        # if i > -10: # run on small datasets (before it gets slow)
+        #     s_types_all = []
+        #     for row in df_cols.select('value', 's_type_col').collect():
+        #         s_type_i = _match_semantic_vals(row['value'], row['s_type_col'], False)
+        #         s_types_all.append(s_type_i[0])
+        #     # get (s_type, count)
+        #     s_types_distinct = sc.parallelize(s_types_all).countByValue().items()
+        # ###
+
+        if i >= -10:
+            ### Spark method
+            df_cols = df_cols.withColumn('s_type_val_add', match_semantic_vals('value', 's_type_col'))  # match uknown col value to semantic type
+            
+            # add to ref set with Spark
+            df_cols = df_cols.select('*', 's_type_val_add.s_type_val')
+            df_cols = df_cols.select('*', 's_type_val_add.add')
+            s_types_distinct = df_cols.select('s_type_val').rdd.map(lambda x: x['s_type_val']).countByValue().items()
+            # for row in df_cols.filter('add == True').select('value', 's_type_val').distinct().collect():
+            #     if len(ref_set_vals[row['s_type_val']]) < 30:
+            #         # print('ADD')
+            #         # print(row['s_type_val'], 'row:', ref_set_vals[row['s_type_val']][-5:], 'val:', row['value'])
+            #         ref_set_vals[row['s_type_val']].append(row['value'])
+            #     else:
+            #         break
+
+        # # DEBUG
+        # df_test = df_cols.groupby('s_type_col', 'value', 's_type_val', 'add').count()
+        # df_test = df_test.sort('count', ascending=False)
+        # print()
+        # print('25 top vals')
+        # df_test.show(25)
+        # print('s_type_val different than s_type_col')
+        # df_test.filter('s_type_val != s_type_col').show(25) 
+        # ###
 
         ds_dict = {
-            'column_name': col,
+            'column_name': ta_path[i],
             'semantic_types': []
         }
         for s_type, count in s_types_distinct:
@@ -284,14 +325,12 @@ def run(gz_paths_cols: List[Tuple[str, str]], ref_set_cols, ref_set_vals):
                     'label': s_type,
                     'count': count
                 })
-        if gz_paths[i] not in master_dct:
-            master_dct[gz_paths[i]] = {}
-        master_dct[gz_paths[i]].update({col: ds_dict})
+        master_lst.append(ds_dict)
 
-        print('gz_paths[i]:', {gz_paths[i]: master_dct[gz_paths[i]]})
+        print('ta_path[i]:', ds_dict)
 
-        with open("results_similarities/master_dct.json", "w") as json_file:
-            json.dump(master_dct, json_file, indent=4)
+        with open("results_similarities/master_dct_0.json", "w") as json_file:
+            json.dump(master_lst, json_file, indent=4)
 
         cache_col_name.clear()
         cache_col_val.clear()
@@ -305,7 +344,7 @@ if __name__ == '__main__':
     if ds_path is None:
         print('No base ds path')
         exit()
-    gz_paths_cols: List[Tuple[str, str]] = get_task2_dfs(ds_path)
+    gz_paths_cols: List[Tuple[str, str, str]] = get_task2_dfs(ds_path)
     # print(gz_paths_cols[:10])
 
     ref_set_cols = collect(
